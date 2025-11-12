929cd35d76bc9e4bd55bf2c1df2fed37b9fe0ec9
Anirudh <36654691+oflakes@users.noreply.github.com>
2025-11-12 13:57:07 +0530

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/anirudhb4/HypeFlix/HypeFlixApplication.java
@@ -0,0 +1,13 @@
+package com.anirudhb4.HypeFlix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HypeFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HypeFlixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anirudhb4/HypeFlix/HypeFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anirudhb4.HypeFlix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HypeFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
