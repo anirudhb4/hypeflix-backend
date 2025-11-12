b90c40bea03b3ad401cfc0226c59da8c77406ba7
Anirudh <36654691+oflakes@users.noreply.github.com>
2025-11-12 14:27:15 +0530

    added repository layer

---
+++ src/main/java/com/anirudhb4/HypeFlix/repository/HypePointRepository.java
@@ -0,0 +1,7 @@
+package com.anirudhb4.HypeFlix.repository;
+
+import com.anirudhb4.HypeFlix.model.HypePoint;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HypePointRepository extends JpaRepository<HypePoint,Long> {
+}
